38c0ed520505f6bc699df972f944b70daa6b40aa
Kuester.Lukas <Kuester.Lukas@HERTZ.local>
2017-12-21 12:50:42 +0100

    Angefangen mit der Navigationbar

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php include 'include/head.php'; ?>
 
+<ul class="oberleiste">
+  <li class="dropdown">
+    <a href="checklisten_uebersicht.php">Checklisten-Übersicht</a>
+  </li>
+  <li class="dropdown">
+    <a href="schueler.php"> Schüler verwalten</a>
+  </li>
+  <li class="dropdown">
+    <a href="klasse.php"> Klassen verwalten</a>
+  </li>
+  <li class="dropdown">
+    <a href="klasse-uebersicht.php">Klassen anzeigen</a>
+  </li>
+</ul>
+<div class="dropdown-inhalt">
+  <p>
+    Test
+  </p>
+</div>
 <?php include 'include/footer.php'; ?>
