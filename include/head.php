68d0598b5736e6f91a66e66e8ad002ee26da706b
Kry.Florian <Kry.Florian@HERTZ.local>
2018-01-10 12:21:05 +0100

    Navigation Bar Style gefixed

--- include/head.php
+++ include/head.php
@@ -10,3 +10,21 @@
 		<title>Checkliste</title>
 	</head>
 	<body>
+		<ul class="oberleiste">
+		  <li class="dropdown">
+		    <a href="checklisten_uebersicht.php">Checklisten-Übersicht</a>
+
+				<div class="dropdown-inhalt">
+				  <a href="checkliste.php">Checkliste</a>
+				</div>
+		  </li>
+		  <li class="dropdown">
+		    <a href="schueler.php"> Schüler verwalten</a>
+		  </li>
+		  <li class="dropdown">
+		    <a href="klasse.php"> Klassen verwalten</a>
+		  </li>
+		  <li class="dropdown">
+		    <a href="klasse-uebersicht.php">Klassen anzeigen</a>
+		  </li>
+		</ul>
